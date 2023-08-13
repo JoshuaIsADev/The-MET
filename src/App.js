0c195cc6c9a46887926d0d38202242b39bec481e
JoshuaIsADev <JoshuaIsADev@gmail.com>
2023-08-13 10:49:02 -0700

    updated ImageList component that shows number of images searched

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import { useState } from 'react';
 import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
 import searchObjects from './api';
 
 function App() {
+  const [images, setImages] = useState([]);
+
   const handleSubmit = async (term) => {
     const result = await searchObjects(term);
 
-    console.log(result);
+    setImages(result);
   };
 
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
     </div>
   );
 }
